eca063489132856c48b39a8c869adc93ba953c00
ARBIYAN12 <148713185+Arbiyansah12345@users.noreply.github.com>
2023-11-28 03:47:55 -0800

    Add files via upload

---
+++ 02 type data/java script/app.java
@@ -0,0 +1,75 @@
+public class app {
+    run | debug
+    public static void man  (String[] args) throws Exception{
+// type data di java
+// integer, byte, short, long, double, float, char, boolean
+
+//integer (satuan)
+int i = 10;
+System.out.println("==========byte========")
+System.out.println("nilai integer i =" + i);
+System.out.println("nilai MAX : " + integer.MAX_VALUE);
+System.out.println("nilai MIN : " + integer.MIN_VALUE);
+System.out.println("besar integer : " + integer.SIZE + "bit");
+
+///byte
+byte b = 10;
+System.out.println("==========byte========")
+System.out.println("nilai byte b =" + i);
+System.out.println("nilai MAX : " + byte.MAX_VALUE);
+System.out.println("nilai MIN : " + byte.MIN_VALUE);
+System.out.println("besar byte : " + byte.SIZE + "bit");
+
+//short
+short s = 10;
+System.out.println("==========short========")
+System.out.println("nilai short i =" + i);
+System.out.println("nilai MAX : " + short.MAX_VALUE);
+System.out.println("nilai MIN : " + short.MIN_VALUE);
+System.out.println("besar short : " + short.SIZE + "bit");
+
+//long
+long l = 10;
+System.out.println("==========long========")
+System.out.println("nilai long i =" + i);
+System.out.println("nilai MAX : " + long.MAX_VALUE);
+System.out.println("nilai MIN : " + long.MIN_VALUE);
+System.out.println("besar long : " + long.SIZE + "bit");
+
+//double 
+double d = 10;
+System.out.println("==========double========")
+System.out.println("nilai double i =" + i);
+System.out.println("nilai MAX : " + double.MAX_VALUE);
+System.out.println("nilai MIN : " + double.MIN_VALUE);
+System.out.println("besar double : " + double.SIZE + "bit");
+
+//float
+float f = 10;
+System.out.println("==========float========")
+System.out.println("nilai float i =" + i);
+System.out.println("nilai MAX : " + float.MAX_VALUE);
+System.out.println("nilai MIN : " + float.MIN_VALUE);
+System.out.println("besar float : " + float.SIZE + "bit");
+
+//char
+char c = 10;
+System.out.println("==========char========")
+System.out.println("nilai char i =" + i);
+System.out.println("nilai MAX : " + char.MAX_VALUE);
+System.out.println("nilai MIN : " + char.MIN_VALUE);
+System.out.println("besar char : " + char.SIZE + "bit");
+
+//boolean
+boolean b = 10;
+System.out.println("==========boolean========")
+System.out.println("nilai boolean i =" + i);
+System.out.println("nilai MAX : " + boolean.MAX_VALUE);
+System.out.println("nilai MIN : " + boolean.MIN_VALUE);
+System.out.println("besar boolean : " + boolean.SIZE + "bit");
+
+
+
+    }
+
+} 
